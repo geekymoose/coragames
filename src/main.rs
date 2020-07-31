1993dfd963d3e64359ef138f27c42088171b14c0
geekymoose <constantin.masson@umontreal.ca>
2020-07-31 02:01:14 +0200

    Some really basic stuff (or more like a todo list as a comment :p)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+struct Player {
+    x: u32,
+    y: u32,
+}
+
+struct Tilemap {
+    map: [u32; 200], // TMP hardcoded values
+}
+
+struct game {
+    tilemap: Tilemap,
+    players: [Player; 5], // TMP hardcoded values
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("--- Cora game server starts ---");
+
+    let turn_duration_seconds = 1.0;
+
+    loop {
+        // Message Connect / Disconnect player
+        // 
+        // Create each player "view" info
+        // Ask each player for action (send "view" info)
+        // Wait until "turn duration" (sleep thread for)
+        // Skipp player if not received on time
+        // Apply player actions (update game)
+    }
+
+    println!("--- Cora game server stops ---");
 }
