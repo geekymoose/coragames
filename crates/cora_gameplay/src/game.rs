ce306134d83bee2ccf7f8172fb9039fc2918f67d
geekymoose <geekymoose@protonmail.com>
2024-01-12 19:14:52 +0100

    Improve the player module

--- crates/cora_gameplay/src/game.rs
+++ crates/cora_gameplay/src/game.rs
@@ -1,6 +1,7 @@
 use crate::action::Action;
+use crate::player::Player;
 use crate::terrain::Config;
-use crate::{player::Player, terrain::Grid};
+use crate::terrain::Grid;
 use std::collections::HashMap;
 
 // Used for the unique player IDs (it's not the best way but OK for now)
@@ -22,24 +23,12 @@ impl<'a> Game<'a> {
     }
 
     pub fn register_player_action_turn(player: &mut Player, player_action: Action) {
-        player.action = Some(player_action);
+        todo!("Not Implemented");
     }
 
     pub fn apply_turn(&mut self) {
         self.players.values_mut().for_each(|p: &mut Player| {
-            p.stats.update_played_one_turn(&p.action);
-
-            match p.action.take() {
-                Some(action) => match action {
-                    Action::Move(Direction) => todo!(),
-                    Action::Attack(Direction) => todo!(),
-                    Action::Interact(Direction) => todo!(),
-                },
-                None => {
-                    // The player did not requested any action
-                    // Too bad for them
-                }
-            }
+            p.apply_turn_action();
         });
 
         self.current_turn += 1;
--- crates/cora_gameserver/src/main.rs
+++ crates/cora_gameserver/src/main.rs
@@ -1,30 +1,34 @@
+use cora_gameplay::game::Game;
 use std::{thread, time::Duration};
 
 const TURN_DURECTION_SEC: u64 = 1;
 
 fn main() {
-    println!("--- Cora game server starts ---");
+    println!("--- Cora GameServer starts ---");
 
     let config: cora_gameplay::terrain::Config = cora_gameplay::terrain::Config {
         width: 100,
         height: 100,
     };
 
-    let mut game: cora_gameplay::game::Game = cora_gameplay::game::Game::new(&config);
+    let mut game: Game = Game::new(&config);
+
+    // game.spawn_player("player1");
+    // game.spawn_player("player2");
 
     loop {
         println!("Playing one turn...");
+
+        // let turn_action_requests = game.start_turn();
+        // for a in turn_action_requests {
+        //      TODO send action
+        // }
+        // TODO Listen for action during TURN_DURECTION_SEC seconds
+        // game.register_player_action_turn
+        // game.end_turn()
         thread::sleep(Duration::from_secs(TURN_DURECTION_SEC));
         game.apply_turn();
-
-        // Message Connect / Disconnect player
-        //
-        // Create each player "view" info
-        // Ask each player for action (send "view" info)
-        // Wait until "turn duration" (sleep thread for)
-        // Skipp player if not received on time
-        // Apply player actions (update game)
     }
 
-    println!("--- Cora game server stops ---");
+    println!("--- Cora GameServer stops ---");
 }
