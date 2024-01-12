df2ed665805d9a10d4c302297f97db4c6cecad4b
geekymoose <geekymoose@protonmail.com>
2024-01-12 15:51:27 +0100

    Add the empty cora_gameagent

---
+++ crates/cora_gameagent/src/main.rs
@@ -0,0 +1,11 @@
+// TODO WIP
+
+fn main() {
+    println!("Cora GameAgent stars...");
+
+    // TODO Receive turn info
+    // TODO Compute action to make
+    // TODO Send response to the server
+
+    println!("Cora GameAgent stops...");
+}
