b490c24dc5dac0f2cbcbd320d216a1fe8dd4933c
geekymoose <geekymoose@protonmail.com>
2024-01-15 17:58:09 +0100

    Add some basic unit implementation such as take_damage

--- crates/cora_gameplay/src/action.rs
+++ crates/cora_gameplay/src/action.rs
@@ -1,3 +1,5 @@
+use crate::{terrain::Grid, unit::Unit};
+
 pub(crate) enum Action {
     Move(Direction),
     Attack(Direction),
@@ -9,4 +11,12 @@ pub(crate) enum Direction {
     Down,
     Left,
     Right,
+}
+
+pub(crate) fn apply_action(action: Action, location: &mut Grid, unit: &mut Unit) {
+    match action {
+        Action::Move(direction) => unit.movement(direction),
+        Action::Attack(direction) => unit.attack(direction),
+        Action::Interact(direction) => unit.interact(direction),
+    }
 }
--- crates/cora_gameplay/src/unit.rs
+++ crates/cora_gameplay/src/unit.rs
@@ -1,3 +1,55 @@
+use crate::action::Direction;
+
 pub(crate) struct Unit {
-    pub(crate) health: u32,
+    health: u32,
+    strength: u32,
+}
+
+struct DamageStat {
+    effective_damage: u32,
+    extra_ammount: u32,
+    killshot: bool,
+}
+
+impl Unit {
+    pub(crate) fn movement(&self, direction: Direction) {
+        todo!("Not Implemented");
+    }
+
+    pub(crate) fn attack(&self, direction: Direction) {
+        todo!("Not Implemented");
+    }
+
+    pub(crate) fn interact(&self, direction: Direction) {
+        todo!("Not Implemented");
+    }
+
+    pub(crate) fn is_alive(&self) -> bool {
+        self.health > 0
+    }
+
+    pub(crate) fn is_dead(&self) -> bool {
+        !self.is_alive()
+    }
+
+    fn take_damage(&mut self, damages: u32) -> DamageStat {
+        let mut damage_applied: DamageStat = DamageStat {
+            effective_damage: 0,
+            extra_ammount: 0,
+            killshot: false,
+        };
+
+        if damages >= self.health {
+            damage_applied.effective_damage = self.health;
+            damage_applied.extra_ammount = damages - self.health;
+            damage_applied.killshot = true;
+        } else {
+            damage_applied.effective_damage = damages;
+            damage_applied.extra_ammount = 0;
+            damage_applied.killshot = false;
+        }
+
+        self.health -= damage_applied.effective_damage;
+        return damage_applied;
+    }
 }
