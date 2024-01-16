a4c4ccaf67d9c0d839b8aee59e2a225a25dfe1db
geekymoose <constantin.masson@umontreal.ca>
2024-01-16 21:04:51 +0100

    Create a wip getneighbors methods for the grid

--- crates/cora_gameplay/src/terrain.rs
+++ crates/cora_gameplay/src/terrain.rs
@@ -46,12 +46,51 @@ impl Grid {
         self.cells.reserve(self.config.width);
 
         for x in 0..self.config.width {
-            self.cells.reserve(self.config.height);
+            self.cells[x].reserve(self.config.height);
             for y in 0..self.config.height {
                 let cell = Cell::new(x, y, EnvironmentType::Movable);
                 self.cells[x].push(cell);
             }
         }
+    }
+
+    fn get_cell_at_pos(&self, x: usize, y: usize) -> Option<&Cell> {
+        if self.is_valid_coordinates(x, y) {
+            return Some(&self.cells[x][y]);
+        }
+        return None;
+    }
+
+    fn get_neighbor_at_direction(&self, x: usize, y: usize, direction: Direction) -> Option<&Cell> {
+        if !self.is_valid_coordinates(x, y) {
+            return None;
+        }
+
+        let mut dir_x = x;
+        let mut dir_y = y;
+
+        match direction {
+            Direction::Up => {
+                dir_y += 1;
+            }
+            Direction::Down => {
+                dir_y -= 1;
+            }
+            Direction::Left => {
+                dir_x -= 1;
+            }
+            Direction::Right => {
+                dir_x += 1;
+            }
+        }
+
+        return self.get_cell_at_pos(dir_x, dir_y);
+    }
+
+    fn is_valid_coordinates(&self, x: usize, y: usize) -> bool {
+        let valid_x = x < self.config.width;
+        let valid_y = y < self.config.height;
+        return valid_x && valid_y;
     }
 }
 
