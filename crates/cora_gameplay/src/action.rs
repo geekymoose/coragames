599a2ef49a443f242123c209920dfcd5bdff1d41
geekymoose <geekymoose@protonmail.com>
2024-01-29 18:23:32 +0100

    Work on the movement action on the grid (WIP)

--- crates/cora_gameplay/src/action.rs
+++ crates/cora_gameplay/src/action.rs
@@ -1,4 +1,7 @@
-use crate::terrain::Direction;
+use crate::{
+    terrain::{self, Direction, Grid},
+    unit::Unit,
+};
 
 #[derive(Debug)]
 pub enum Action {
@@ -7,9 +10,14 @@ pub enum Action {
     Interact(Direction),
 }
 
-pub(crate) fn apply_action(action: Action) {
+pub(crate) fn apply_action(action: Action, unit: &mut Unit, terrain: &mut Grid) {
     match action {
-        Action::Move(_) => todo!("Not Implemented"),
+        Action::Move(direction) => {
+            match terrain::move_unit(unit, terrain, direction) {
+                Ok(_) => println!("Move action applied"),
+                Err(msg) => println!("Unable to applie the move action: {}", msg),
+            }
+        }
         Action::Attack(_) => todo!("Not Implemented"),
         Action::Interact(_) => todo!("Not Implemented"),
     }
