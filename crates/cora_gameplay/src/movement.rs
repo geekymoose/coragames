72a438d31383cb411a8dcbfcaf883271942fd245
geekymoose <geekymoose@protonmail.com>
2024-02-06 16:00:35 +0100

    Update the movement method

--- crates/cora_gameplay/src/movement.rs
+++ crates/cora_gameplay/src/movement.rs
@@ -3,35 +3,43 @@ use crate::{direction::Direction, grid_map::Grid, grid_unit::GridUnit};
 pub(crate) fn move_unit(
     unit: GridUnit,
     grid: &mut Grid,
-    direction: Direction,
+    direction: &Direction,
 ) -> Result<GridUnit, &'static str> {
-    todo!("WIP Not Implemented");
-    /*
-    let dest_coord = match terrain.get_neighbor_coord_at_direction(x, y, &direction) {
-        Some(coord) => coord,
-        None => return Err("The requested move has reach the border and cannot be applied"),
+    let dest = match grid.neighbor_at_direction(unit.x(), unit.y(), direction) {
+        Some(cell) => cell,
+        None => return Err(
+            "Unable to get the neighbor cell for this direction (you may have reach the border)",
+        ),
     };
 
-    if (!terrain.is_cell_free_at_coord(dest_coord.x, dest_coord.y)) {
-        return Err("Cannot move the Unit, the dest cell has already a Unit");
+    if dest.has_unit() {
+        return Err("Unable to move on a cell that already has a Unit on it");
+    }
+    if !dest.is_walkable() {
+        return Err("Unable to move on a cell that is not walkable");
     }
 
-    let dest_cell = match terrain.get_mut_neighbor_at_direction(x, y, direction) {
-        Some(value) => value,
-        None => return Err("Unable to get the destination cell where to move the unit"),
+    let src = match grid.cell_at_pos_mut(unit.x(), unit.y()) {
+        Some(cell) => cell,
+        None => return Err("Unable to get the requested cell from the Grid"),
     };
 
-    let dest_x = dest_cell.x();
-    let dest_y = dest_cell.y();
+    let unit = match src.remove_unit() {
+        Some(unit) => unit,
+        None => {
+            return Err("Internal error: the src Unit should have the requested Unit before moving")
+        }
+    };
 
-    match terrain.remove_unit_from_coord(dest_x, dest_y) {
-        Ok(unit_option) => match unit_option {
-            Some(value) => value,
-            None => return Err("The grid cell has no unit at this position"),
-        },
+    let dest = match grid.neighbor_at_direction_mut(unit.x(), unit.y(), direction) {
+        Some(cell) => cell,
+        None => return Err(
+            "Unable to get the neighbor cell for this direction (you may have reach the border)",
+        ),
+    };
+
+    match dest.place_unit(unit) {
+        Ok(unit) => return Ok(unit),
         Err(msg) => return Err(msg),
-    };
-    return Ok(());
-
-    */
+    }
 }
